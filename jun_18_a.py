b0a7174b5271c9f202dabc8daccd0f41ef9a2583
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-18 23:52:18 +0800

    Create jun_18_a.py

---
+++ jun_18_a.py
@@ -0,0 +1,7 @@
+word_data = {
+    "la boîte à outil": ("toolbox", "Elle a ouvert la boîte à outil pour réparer la chaise."),
+    "les personnages": ("characters", "Les personnages principaux de ce film sont très intéressants."),
+    "les consignes": ("instructions", "Lis bien les consignes avant de commencer l'exercice."),
+    "télécharger": ("to download", "Tu peux télécharger l'application sur le site officiel."),
+    "imprimer": ("to print", "Je vais imprimer le document pour la réunion."),
+}
