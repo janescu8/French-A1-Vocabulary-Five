f061424cebdff99653037c54a229cba5b6b8436e
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-20 00:10:12 +0800

    Delete jun_18_a.py

--- jun_18_a.py
+++
@@ -1,7 +0,0 @@
-word_data = {
-    "la boîte à outils": ("toolbox", "Elle a ouvert la boîte à outils pour réparer la chaise."),
-    "les personnages": ("characters", "Les personnages principaux de ce film sont très intéressants."),
-    "les consignes": ("instructions", "Lis bien les consignes avant de commencer l'exercice."),
-    "télécharger": ("to download", "Tu peux télécharger l'application sur le site officiel."),
-    "imprimer": ("to print", "Je vais imprimer le document pour la réunion."),
-}
